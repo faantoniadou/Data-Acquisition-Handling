ec2ccd19ac7d602ed1e6a653c77cf174a445b998
Faidra Antoniadou <63123583+faantoniadou@users.noreply.github.com>
2021-10-18 17:18:43 +0100

    Update checkpoint_4b.py

--- checkpoint_4b.py
+++ checkpoint_4b.py
@@ -19,38 +19,28 @@ pcf.digitalWrite(LED3, True)
 
 # Christmas Lights
 count = 0
-#while count < 4:
-#    count += 1
-#    time.sleep(0.5)
-#    pcf.portWrite(10)
-#    time.sleep(0.5)
-#    pcf.portWrite(5)
-#time.sleep(0.5)
+while count < 4:
+    count += 1
+    time.sleep(0.5)
+    pcf.portWrite(10)
+    time.sleep(0.5)
+    pcf.portWrite(5)
+time.sleep(0.5)
 
-#pcf.portWrite(0) #1111
-#time.sleep(0.5)
-#pcf.portWrite(1) #0111
-# time.sleep(0.5)
+#pcf.portWrite(0)  #1111
+#pcf.portWrite(1)  #0111
 #pcf.portWrite(2)
-# time.sleep(0.5) #1011
-#pcf.portWrite(3) #0011
-# time.sleep(0.5)
-#pcf.portWrite(4) #1101
-# time.sleep(0.5)
+# time.sleep(0.5)  #1011
+#pcf.portWrite(3)  #0011
+#pcf.portWrite(4)  #1101
 #pcf.portWrite(5)  #0101
-#time.sleep(0.5)
-#pcf.portWrite(6) #1001
-#time.sleep(0.5)
-#pcf.portWrite(7) #0001
-#time.sleep(0.5)
-#pcf.portWrite(8) #1110
-#pcf.portWrite(9) #0110
+#pcf.portWrite(6)  #1001
+#pcf.portWrite(7)  #0001
+#pcf.portWrite(8)  #1110
+#pcf.portWrite(9)  #0110
 #pcf.portWrite(10) #1010
 #pcf.portWrite(11) #0010
 #pcf.portWrite(12) #1100
-time.sleep(0.5)
-#pcf.portWrite(4)
-
 
 
 pcf.digitalWrite(LED0, True)
